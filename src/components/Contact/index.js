e7df1d25ccfcceaf21164ab0e4c5ceb66194e901
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-08-02 23:10:48 +0200

    wqorking onn  form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -36,6 +36,7 @@ const Contact = () => {
         }
       )
   }
+
   return (
     <>
       <div className="container contact-page">
@@ -48,9 +49,7 @@ const Contact = () => {
             />
           </h1>
           <p>
-            I am interested in freelance opportunities - especially ambitious or
-            large projects. However, if you have other request or question,
-            don't hesitate to contact me using below form either.
+            I am interested in freelance opportunities 
           </p>
           <div className="contact-form">
             <form ref={form} onSubmit={sendEmail}>
@@ -88,3 +87,28 @@ const Contact = () => {
             </form>
           </div>
         </div>
+        <div className="info-map">
+          Slobodan Gajić,
+          <br />
+          Serbia,
+          <br />
+          Branka RadiČevića 19, 22000 <br />
+          Sremska Mitrovica <br />
+          <br />
+          <span>freelancerslobodan@gmail.com</span>
+        </div>
+        <div className="map-wrap">
+          <MapContainer center={[44.96366, 19.61045]} zoom={13}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[44.96366, 19.61045]}>
+              <Popup>Sloba lives here, come over for a cup of coffee :)</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Contact
