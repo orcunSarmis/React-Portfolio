abd41d0edc2e5146181d50b773c9ce4f0fd5427c
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-08-02 17:27:28 +0200

    adding letters

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,3 +6,25 @@ import LogoTitle from '../../assets/images/logo-s.png'
 import Logo from './Logo'
 import './index.scss'
 
+const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+  
+    const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
+    const jobArray = [
+      'w',
+      'e',
+      'b',
+      ' ',
+      'd',
+      'e',
+      'v',
+      'e',
+      'l',
+      'o',
+      'p',
+      'e',
+      'r',
+      '.',
+    ]
+
+    
